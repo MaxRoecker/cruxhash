61b085e6d674440b6ea9f2aa197f647b7ea83748
Max Naegeler Roecker <max.roecker@gmail.com>
2021-05-06 21:37:11 -0300

    Add hash functions

---
+++ index.ts
@@ -0,0 +1,209 @@
+export type Seed = [number, number];
+
+const defaultSeed: Seed = [0x9dc5, 0x811c];
+
+const hexLUT = Array.from({ length: 256 }, (_, index) => {
+  return ((index >> 4) & 15).toString(16) + (index & 15).toString(16);
+});
+
+/**
+ * Returns a new seed pair from a string.
+ */
+export const getSeed = (str: string): Seed => {
+  const value = hashString(str, defaultSeed);
+  const hex =
+    hexLUT[value >>> 24] +
+    hexLUT[(value >>> 16) & 255] +
+    hexLUT[(value >>> 8) & 255] +
+    hexLUT[value & 255];
+  const s0 = parseInt(hex.substr(0, 4), 16);
+  const s1 = parseInt(hex.substr(4, 4), 16);
+  return [s0, s1];
+};
+
+/**
+ * Hashes a boolean into a unsigned int.
+ */
+export const hashBoolean = (bool: boolean, seed = defaultSeed): number => {
+  return bool ? (0x42108421 ^ seed[0]) >>> 0 : (0x42108420 ^ seed[1]) >>> 0;
+};
+
+/**
+ * Hashes a number into a unsigned int.
+ */
+export const hashNumber = (num: number, seed = defaultSeed): number => {
+  if (Number.isNaN(num)) return (0x42108425 ^ seed[0]) >>> 0;
+  if (num === Number.POSITIVE_INFINITY) return (0x42108426 ^ seed[0]) >>> 0;
+  if (num === Number.NEGATIVE_INFINITY) return (0x42108426 ^ seed[1]) >>> 0;
+  let hashed = (num | 0) ^ seed[0];
+  hashed -= hashed << 6;
+  hashed ^= hashed >>> 17;
+  hashed -= hashed << 9;
+  hashed ^= hashed << 4;
+  hashed -= hashed << 3;
+  hashed ^= hashed << 10;
+  hashed ^= hashed >>> 15;
+  return hashed >>> 0;
+};
+
+/**
+ * Hashes a string into a unsigned int.
+ */
+export const hashString = (str: string, seed = defaultSeed): number => {
+  const length = str.length;
+  let i = 0;
+  let t0 = 0;
+  let t1 = 0;
+  let v0 = seed[0] | 0;
+  let v1 = seed[1] | 0;
+
+  while (i < length) {
+    let char = str.charCodeAt(i);
+    if (char < 128) {
+      v0 ^= char;
+    } else if (char < 2048) {
+      v0 ^= (char >> 6) | 192;
+      t0 = v0 * 403;
+      t1 = v1 * 403;
+      t1 += v0 << 8;
+      v1 = (t1 + (t0 >>> 16)) & 65535;
+      v0 = t0 & 65535;
+      v0 ^= (char & 63) | 128;
+    } else if (
+      (char & 64512) == 55296 &&
+      i + 1 < length &&
+      (str.charCodeAt(i + 1) & 64512) == 56320
+    ) {
+      char = 65536 + ((char & 1023) << 10) + (str.charCodeAt(++i) & 1023);
+      v0 ^= (char >> 18) | 240;
+      t0 = v0 * 403;
+      t1 = v1 * 403;
+      t1 += v0 << 8;
+      v1 = (t1 + (t0 >>> 16)) & 65535;
+      v0 = t0 & 65535;
+      v0 ^= ((char >> 12) & 63) | 128;
+      t0 = v0 * 403;
+      t1 = v1 * 403;
+      t1 += v0 << 8;
+      v1 = (t1 + (t0 >>> 16)) & 65535;
+      v0 = t0 & 65535;
+      v0 ^= ((char >> 6) & 63) | 128;
+      t0 = v0 * 403;
+      t1 = v1 * 403;
+      t1 += v0 << 8;
+      v1 = (t1 + (t0 >>> 16)) & 65535;
+      v0 = t0 & 65535;
+      v0 ^= (char & 63) | 128;
+    } else {
+      v0 ^= (char >> 12) | 224;
+      t0 = v0 * 403;
+      t1 = v1 * 403;
+      t1 += v0 << 8;
+      v1 = (t1 + (t0 >>> 16)) & 65535;
+      v0 = t0 & 65535;
+      v0 ^= ((char >> 6) & 63) | 128;
+      t0 = v0 * 403;
+      t1 = v1 * 403;
+      t1 += v0 << 8;
+      v1 = (t1 + (t0 >>> 16)) & 65535;
+      v0 = t0 & 65535;
+      v0 ^= (char & 63) | 128;
+    }
+    t0 = v0 * 403;
+    t1 = v1 * 403;
+    t1 += v0 << 8;
+    v1 = (t1 + (t0 >>> 16)) & 65535;
+    v0 = t0 & 65535;
+
+    i += 1;
+  }
+
+  return ((v1 << 16) >>> 0) + v0;
+};
+
+/**
+ * Hashes a symbol into a unsigned int.
+ */
+export const hashSymbol = (sym: symbol, seed = defaultSeed): number => {
+  return hashString(sym.toString(), seed);
+};
+
+/**
+ * Hashes a bigint into a unsigned int.
+ */
+export const hashBigInt = (int: bigint, seed = defaultSeed): number => {
+  return hashString(int.toString() + 'n', seed);
+};
+
+/**
+ * Hashes a function into a unsigned int considering its string representation.
+ */
+// eslint-disable-next-line @typescript-eslint/ban-types
+export const hashFunction = (fun: Function, seed = defaultSeed): number => {
+  return hashString(fun.toString(), seed);
+};
+
+/**
+ * Hashes an iterable into a unsigned int considering its content.
+ */
+export const hashIterable = (
+  itr: Iterable<unknown>,
+  seed = defaultSeed
+): number => {
+  const arr = Array.isArray(itr) ? itr : Array.from(itr);
+  let hashed = seed[0];
+  for (let i = 0; i < arr.length; i += 1) {
+    hashed ^= hash(arr[i], seed);
+  }
+  return hashed >>> 0;
+};
+
+/**
+ * Hashes an object into a unsigned int considering `Object.entries`.
+ */
+// eslint-disable-next-line @typescript-eslint/ban-types
+export const hashObject = (obj: object | null, seed = defaultSeed): number => {
+  if (obj === null) {
+    return 0x42108422 ^ seed[1];
+  } else {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const target: any = obj;
+    if (typeof target.hashCode === 'function') {
+      return target.hashCode() >>> 0;
+    } else if (target[Symbol.iterator] != undefined) {
+      return hashIterable(target, seed);
+    } else if (typeof obj.valueOf === 'function' && obj.valueOf() !== obj) {
+      return hash(obj.valueOf(), seed);
+    } else {
+      const entries = Object.entries(obj);
+      const sorted = entries.sort((a, b) => a[0].localeCompare(b[0]));
+      return hashIterable(sorted, seed);
+    }
+  }
+};
+
+/**
+ * Hashes an value into a unsigned int.
+ */
+export const hash = (value: unknown, seed = defaultSeed): number => {
+  switch (typeof value) {
+    case 'undefined':
+      return 0x42108423 ^ seed[0];
+    case 'boolean':
+      return hashBoolean(value, seed);
+    case 'number':
+      return hashNumber(value, seed);
+    case 'string':
+      return hashString(value, seed);
+    case 'symbol':
+      return hashSymbol(value, seed);
+    case 'bigint':
+      return hashBigInt(value, seed);
+    case 'function':
+      return hashFunction(value, seed);
+    case 'object':
+      return hashObject(value, seed);
+    default:
+      return 0x42108424 ^ seed[1];
+  }
+};
---
+++ tests.ts
@@ -0,0 +1,245 @@
+import type { Seed } from './index';
+import {
+  getSeed,
+  hashBoolean,
+  hashNumber,
+  hashString,
+  hashSymbol,
+  hashBigInt,
+  hashFunction,
+  hashIterable,
+  hashObject,
+} from './index';
+
+describe('getSeed tests', () => {
+  it('should return a pair of numbers', () => {
+    const case0 = '';
+    const case1 = 'Smile my dear!';
+    const case2 = "You know you're not fully dressed without one.";
+    expect(getSeed(case0)).toStrictEqual([0x811c, 0x9dc5]);
+    expect(getSeed(case1)).toStrictEqual([0xf452, 0x1e04]);
+    expect(getSeed(case2)).toStrictEqual([0x69a9, 0x5827]);
+  });
+  it('should return the same seed for the same string', () => {
+    const str = 'Smile my dear!';
+    expect(getSeed(str)).toStrictEqual(getSeed(str));
+  });
+});
+
+describe('hashBoolean tests', () => {
+  it('should return different hash for "true" e "false"', () => {
+    expect(hashBoolean(true)).not.toBe(hashBoolean(false));
+  });
+  it('should return the same hash for the same value', () => {
+    expect(hashBoolean(true)).toBe(hashBoolean(true));
+    expect(hashBoolean(false)).toBe(hashBoolean(false));
+  });
+  it('should return different hash for different seeds', () => {
+    const seed: Seed = [0x811c, 0x9dc5];
+    expect(hashBoolean(true, seed)).not.toBe(hashBoolean(true));
+    expect(hashBoolean(false, seed)).not.toBe(hashBoolean(false));
+  });
+});
+
+describe('hashNumber tests', () => {
+  it('should return the same hash for the same value', () => {
+    expect(hashNumber(8)).toBe(hashNumber(8));
+    expect(hashNumber(+0)).toBe(hashNumber(-0));
+    expect(hashNumber(10e10)).toBe(hashNumber(10e10));
+    expect(hashNumber(Number.NaN)).toBe(hashNumber(Number.NaN));
+    expect(hashNumber(Infinity)).toBe(hashNumber(Infinity));
+    expect(hashNumber(-Infinity)).toBe(hashNumber(-Infinity));
+  });
+  it('should return different hash for different seeds', () => {
+    const seed: Seed = [0x811c, 0x9dc5];
+    expect(hashNumber(8, seed)).not.toBe(hashNumber(8));
+    expect(hashNumber(+0, seed)).not.toBe(hashNumber(-0));
+    expect(hashNumber(10e10, seed)).not.toBe(hashNumber(10e10));
+    expect(hashNumber(Number.NaN, seed)).not.toBe(hashNumber(Number.NaN));
+    expect(hashNumber(Infinity, seed)).not.toBe(hashNumber(Infinity));
+    expect(hashNumber(-Infinity, seed)).not.toBe(hashNumber(-Infinity));
+  });
+});
+
+describe('hashString tests', () => {
+  it('should return the same hash for the same value', () => {
+    expect(hashString('')).toBe(hashString(''));
+    expect(hashString('Smile my dear!')).toBe(hashString('Smile my dear!'));
+    expect(hashString('コンニチハ, Hello world, Καλημέρα κόσμε 😀')).toBe(
+      hashString('コンニチハ, Hello world, Καλημέρα κόσμε 😀')
+    );
+  });
+  it('should return different hash for different seeds', () => {
+    const seed: Seed = [0x811c, 0x9dc5];
+    expect(hashString('', seed)).not.toBe(hashString(''));
+    expect(hashString('Smile my dear!', seed)).not.toBe(
+      hashString('Smile my dear!')
+    );
+    expect(
+      hashString('コンニチハ, Hello world, Καλημέρα κόσμε 😀', seed)
+    ).not.toBe(hashString('コンニチハ, Hello world, Καλημέρα κόσμε 😀'));
+  });
+});
+
+describe('hashSymbol tests', () => {
+  it('should return the same hash for the same value', () => {
+    expect(hashSymbol(Symbol.iterator)).toBe(hashSymbol(Symbol.iterator));
+    expect(hashSymbol(Symbol.match)).toBe(hashSymbol(Symbol.match));
+  });
+  it('should return different hash for different seeds', () => {
+    const seed: Seed = [0x811c, 0x9dc5];
+    expect(hashSymbol(Symbol.iterator, seed)).not.toBe(
+      hashSymbol(Symbol.iterator)
+    );
+    expect(hashSymbol(Symbol.match, seed)).not.toBe(hashSymbol(Symbol.match));
+  });
+});
+
+describe('hashBigInt tests', () => {
+  it('should return the same hash for the same value', () => {
+    expect(hashBigInt(8n)).toBe(hashBigInt(8n));
+    expect(hashBigInt(0n)).toBe(hashBigInt(-0n));
+    expect(hashBigInt(100000000000n)).toBe(hashBigInt(100000000000n));
+  });
+  it('should return different hash for different seeds', () => {
+    const seed: Seed = [0x811c, 0x9dc5];
+    expect(hashBigInt(8n, seed)).not.toBe(hashBigInt(8n));
+    expect(hashBigInt(0n, seed)).not.toBe(hashBigInt(-0n));
+    expect(hashBigInt(100000000000n, seed)).not.toBe(hashBigInt(100000000000n));
+  });
+});
+
+describe('hashFunction tests', () => {
+  it('should return the same hash for the same value', () => {
+    expect(hashFunction(() => void 0)).toBe(hashFunction(() => void 0));
+    expect(hashFunction(describe)).toBe(hashFunction(describe));
+    expect(hashFunction(it)).toBe(hashFunction(it));
+  });
+  it('should return different hash for different seeds', () => {
+    const seed: Seed = [0x811c, 0x9dc5];
+    expect(hashFunction(() => void 0, seed)).not.toBe(
+      hashFunction(() => void 0)
+    );
+    expect(hashFunction(describe, seed)).not.toBe(hashFunction(describe));
+    expect(hashFunction(it, seed)).not.toBe(hashFunction(it));
+  });
+});
+
+describe('hashIterable tests', () => {
+  it('should return the same hash for the same value', () => {
+    expect(hashIterable([1, 2, 3])).toBe(hashIterable([1, 2, 3]));
+    expect(
+      hashIterable(
+        new Map([
+          ['a', 1],
+          ['b', 2],
+        ])
+      )
+    ).toBe(
+      hashIterable(
+        new Map([
+          ['a', 1],
+          ['b', 2],
+        ])
+      )
+    );
+    expect(hashIterable(new Set([null, true, 'a']))).toBe(
+      hashIterable(new Set([null, true, 'a']))
+    );
+  });
+  it('should return different hash for different seeds', () => {
+    const seed: Seed = [0x811c, 0x9dc5];
+    expect(hashIterable([1, 2, 3], seed)).not.toBe(hashIterable([1, 2, 3]));
+    expect(
+      hashIterable(
+        new Map([
+          ['a', 1],
+          ['b', 2],
+        ]),
+        seed
+      )
+    ).not.toBe(
+      hashIterable(
+        new Map([
+          ['a', 1],
+          ['b', 2],
+        ])
+      )
+    );
+    expect(hashIterable(new Set([null, true, 'a']), seed)).not.toBe(
+      hashIterable(new Set([null, true, 'a']))
+    );
+  });
+});
+
+describe('hashObject tests', () => {
+  it('should return the same hash for the same value', () => {
+    expect(hashObject(null)).toBe(hashObject(null));
+    expect(hashObject({})).toBe(hashObject({}));
+    expect(hashObject({ a: undefined })).toBe(hashObject({ a: undefined }));
+    expect(hashObject({ a: 1, b: 2n, c: () => void 0, d: Symbol.match })).toBe(
+      hashObject({ b: 2n, a: 1, c: () => void 0, d: Symbol.match })
+    );
+    expect(hashObject(new Date(0))).toBe(hashObject(new Date(0)));
+  });
+  it('should return different hash for different seeds', () => {
+    const seed: Seed = [0x811c, 0x9dc5];
+    expect(hashObject(null, seed)).not.toBe(hashObject(null));
+    expect(hashObject({}, seed)).not.toBe(hashObject({}));
+    expect(hashObject({ a: undefined }, seed)).not.toBe(
+      hashObject({ a: undefined })
+    );
+    expect(
+      hashObject({ a: 1, b: 2n, c: () => void 0, d: Symbol.match }, seed)
+    ).not.toBe(hashObject({ b: 2n, a: 1, c: () => void 0, d: Symbol.match }));
+    expect(hashObject(new Date(0), seed)).not.toBe(hashObject(new Date(0)));
+  });
+  it('should consider the hashCode method', () => {
+    const seed: Seed = [0x811c, 0x9dc5];
+    expect(
+      hashObject({
+        hashCode() {
+          return 0x811c;
+        },
+      })
+    ).toBe(
+      hashObject({
+        hashCode() {
+          return 0x811c;
+        },
+      })
+    );
+    expect(
+      hashObject(
+        {
+          hashCode() {
+            return 0x9dc5;
+          },
+        },
+        seed
+      )
+    ).toBe(
+      hashObject({
+        hashCode() {
+          return 0x9dc5;
+        },
+      })
+    );
+    expect(
+      hashObject(
+        {
+          hashCode() {
+            return 0x811c;
+          },
+        },
+        seed
+      )
+    ).not.toBe(
+      hashObject({
+        hashCode() {
+          return 0x9dc5;
+        },
+      })
+    );
+  });
+});
